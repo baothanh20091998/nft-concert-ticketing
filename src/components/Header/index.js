9b78164ec3ba7a27cf889dd2e24ca163942f6ecb
dolong <dolong2310@gmail.com>
2023-02-26 01:53:22 +0700

    update: update ui home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,11 +9,18 @@ const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx("wrapper")}>
-            <div className={cx("navigation")}>
-                <Link to="/">Home</Link>
-                <Link to="/profile">Profile</Link>
+            <h1 className={cx("logo")}>
+                <Link to="/">
+                    <span>NFT</span>icket
+                </Link>
+            </h1>
+            <div className={cx("action")}>
+                <div className={cx("navigation")}>
+                    <Link to="/">Home</Link>
+                    <Link to="/profile">Profile</Link>
+                </div>
+                <Button>Connect wallet</Button>
             </div>
-            <Button>Connect wallet</Button>
         </header>
     );
 }

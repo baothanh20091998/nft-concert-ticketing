9fa29b2c7b02b49d41cb080c4bb6be3dfbe68029
ThanhHLB <ThanhHLB@coin98.finance>
2023-03-01 10:01:16 +0700

    update function mint

--- src/components/AccountTickets/index.js
+++ src/components/AccountTickets/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useUser } from "../../context";
 import AccountTicketCard from "../AccountTicketCard";
 import styles from "./style.module.scss";
@@ -6,33 +6,37 @@ import styles from "./style.module.scss";
 const AccountTickets = () => {
   const { account } = useUser();
   const [value, setValue] = useState("");
-  const [data, setData] = useState([
-    { hash: account, type: "Diamond" },
-    { hash: account, type: "Gold" },
-    { hash: account, type: "Bronze" },
-    { hash: account, type: "Silver" },
-    { hash: account, type: "Silver" },
-    { hash: account, type: "Bronze" },
-    { hash: account, type: "Diamond" },
-    { hash: account, type: "Diamond" },
-    { hash: account, type: "Gold" },
-  ]);
+  const [data, setData] = useState([]);
   const handleChangeValue = (e) => setValue(e.target.value);
 
-  const renderCard = ({ hash, type }, index) => {
+  const renderCard = ({ ticketId, ticketType }, index) => {
     return (
       <div className="nft_items">
-        <AccountTicketCard hash={hash} id={index + 1} type={type} />
+        <AccountTicketCard
+          ticketId={ticketId}
+          id={index + 1}
+          ticketType={ticketType}
+        />
       </div>
     );
   };
 
   const renderFilteredCard = () => {
     const dataFiltered = data.filter((item) =>
-      item.hash.toLowerCase().includes(value.toLowerCase())
+      item.ticketId.toLowerCase().includes(value.toLowerCase())
     );
     return dataFiltered.map(renderCard);
   };
+
+  useEffect(() => {
+    const tickets = JSON.parse(localStorage.getItem("tikets") || "[]");
+    if (tickets.length > 0) {
+      const i = tickets.findIndex((item) => item.address == account);
+      if (i !== -1) {
+        setData(tickets[i].tickets);
+      }
+    }
+  }, []);
 
   return (
     <div className={styles.wrapper}>
